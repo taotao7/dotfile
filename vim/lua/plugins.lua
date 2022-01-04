f0594fe023b4ac3868962eca3cafe6eb318c144f
taotao7 <moca_tao7@foxmail.com>
2022-01-04 10:43:38 +0800

    feat: change vim config to lua

---
+++ vim/lua/plugins.lua
@@ -0,0 +1,13 @@
+return require('packer').startup(function()
+  use {'liuchengxu/vista.vim'}
+  use {'neoclide/coc.nvim', branch = 'release'}
+  use {'luochen1990/rainbow'}
+  use {'dracula/vim', as = 'dracula'}
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+  use {'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} }}
+  use {'numToStr/Comment.nvim', config = function() require('Comment').setup() end}
+  use {'tpope/vim-fugitive'}
+  use {'glepnir/dashboard-nvim'}
+  use {'glepnir/galaxyline.nvim', branch = 'main', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
+end)
+
