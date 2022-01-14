b9d4255d7e4b0b3a1fbb311f8f0975c565a57721
taotao7 <moca_tao7@foxmail.com>
2022-01-14 18:21:14 +0800

    fix: some error

--- vim/lua/lua/plugins.lua
+++ vim/lua/lua/plugins.lua
@@ -6,9 +6,10 @@ return require('packer').startup(function()
   use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
   use {'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} }}
   use {'numToStr/Comment.nvim', config = function() require('Comment').setup() end}
-  use {'vim-airline/vim-airline'}
-  use {'vim-airline/vim-airline-themes'}
   use {'tpope/vim-fugitive'}
   use {'glepnir/dashboard-nvim'}
+  use {'glepnir/galaxyline.nvim', branch = 'main', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
+  use {'SmiteshP/nvim-gps', requires = 'nvim-treesitter/nvim-treesitter' }
+  use {'sainnhe/everforest'}
 end)
 
