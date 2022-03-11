091942f9af9f596909b3c9dae7777f050dc037f0
taotao7 <moca_tao7@foxmail.com>
2022-03-11 09:40:30 +0800

    feat: update packer plugin config

--- vim/lua/lua/plugins.lua
+++ vim/lua/lua/plugins.lua
@@ -1,6 +1,6 @@
 return require('packer').startup(function()
-  use {'vim-airline/vim-airline', branch= 'main' ,event = 'VimEnter'}
-  use {'vim-airline/vim-airline-themes', requires = { {'vim-airline/vim-airline'} }}
+  use {'vim-airline/vim-airline-themes', event = 'VimEnter'}
+  use {'vim-airline/vim-airline', event = 'VimEnter', after = 'vim-airline-themes'}
   use {'liuchengxu/vista.vim'}
   use {'neoclide/coc.nvim', branch = 'release'}
   use {'luochen1990/rainbow'}
@@ -13,7 +13,7 @@ return require('packer').startup(function()
   use {'glepnir/dashboard-nvim'}
   use {'morhetz/gruvbox'}
   -- use {'glepnir/galaxyline.nvim', branch = 'main', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
-  use {'sainnhe/everforest'}
+  -- use {'sainnhe/everforest'}
   -- use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
   -- use {'folke/tokyonight.nvim'}
 end)
