0e1638bd8630554862cbdefbab183cc1f494af1d
xt01102058 <xt01102058@alibaba-inc.com>
2023-02-14 14:23:09 +0800

    feat: update lunarvim config

--- vim/lunarvim/config.lua
+++ vim/lunarvim/config.lua
@@ -3,7 +3,8 @@ lvim.log.level = "warn"
 lvim.format_on_save = true
 -- vim.cmd('set background=light')
 -- lvim.colorscheme = "github_dimmed"
-lvim.colorscheme = "everforest"
+lvim.colorscheme = "onedark"
+lvim.builtin.lualine.options.theme = "onedark"
 -- vim.background = "light"
 -- vim.g.gruvbox_bold = 1;
 -- vim.g.gruvbox_italic = 1;
@@ -15,14 +16,14 @@ vim.opt.wrap = true
 -- wsl2
 if vim.fn.has "wsl" == 1 then
   vim.g.clipboard = {
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    },
+      copy = {
+          ["+"] = "win32yank.exe -i --crlf",
+          ["*"] = "win32yank.exe -i --crlf",
+      },
+      paste = {
+          ["+"] = "win32yank.exe -o --lf",
+          ["*"] = "win32yank.exe -o --lf",
+      },
   }
 end
 
@@ -49,12 +50,12 @@ lvim.leader = ","
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.alpha.dashboard.section.header.val = {
-  " ████████╗ █████╗  ██████╗       ███████╗",
-  " ╚══██╔══╝██╔══██╗██╔═══██╗      ██╔════╝",
-  "    ██║   ███████║██║   ██║█████╗███████╗",
-  "    ██║   ██╔══██║██║   ██║╚════╝╚════██║",
-  "    ██║   ██║  ██║╚██████╔╝      ███████║",
-  "    ╚═╝   ╚═╝  ╚═╝ ╚═════╝       ╚══════╝",
+    " ████████╗ █████╗  ██████╗       ███████╗",
+    " ╚══██╔══╝██╔══██╗██╔═══██╗      ██╔════╝",
+    "    ██║   ███████║██║   ██║█████╗███████╗",
+    "    ██║   ██╔══██║██║   ██║╚════╝╚════██║",
+    "    ██║   ██║  ██║╚██████╔╝      ███████║",
+    "    ╚═╝   ╚═╝  ╚═╝ ╚═════╝       ╚══════╝",
 }
 lvim.builtin.terminal.active = false
 lvim.builtin.nvimtree.setup.view.side = "left"
@@ -63,8 +64,8 @@ lvim.builtin.dap.active = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash", "c", "cpp", "javascript", "json", "lua", "python", "typescript", "tsx",
-  "css", "rust", "java", "yaml"
+    "bash", "c", "cpp", "javascript", "json", "lua", "python", "typescript", "tsx",
+    "css", "rust", "java", "yaml"
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -80,7 +81,10 @@ lvim.builtin.indentlines.options.show_current_context_start = true
 
 -- plugins
 lvim.plugins = {
-  { 'sainnhe/everforest' },
+    { 'sainnhe/everforest' },
+    { 'sainnhe/sonokai' },
+    { 'joshdick/onedark.vim' }
+
 }
 
 -- lsp
@@ -88,21 +92,21 @@ lvim.plugins = {
 -- formatters
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  {
-    command = 'prettier',
-    filetypes = {
-      "typescript", "javascript", "typescriptreact", "javascriptreact"
+    {
+        command = 'prettier',
+        filetypes = {
+            "typescript", "javascript", "typescriptreact", "javascriptreact"
+        }
     }
-  }
 }
 
 -- linter
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  {
-    command = 'eslint',
-    filetypes = {
-      "typescript", "javascript", "typescriptreact", "javascriptreact"
+    {
+        command = 'eslint',
+        filetypes = {
+            "typescript", "javascript", "typescriptreact", "javascriptreact"
+        }
     }
-  }
 }
