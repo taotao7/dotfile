0a96d9e39d3712437a21540b668580ade6408a6a
taotao7 <taotao7@foxmail.com>
2023-08-14 10:48:23 +0800

    update node debug

--- vim/lunarvim/config.lua
+++ vim/lunarvim/config.lua
@@ -249,6 +249,32 @@ dap.configurations.go = {
   },
 }
 
+-- node
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = { mason_path .. 'packages/node-debug2-adapter/out/src/nodeDebug.js' },
+}
+dap.configurations.javascript = {
+  {
+    name = 'Launch',
+    type = 'node2',
+    request = 'launch',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+  {
+    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+    name = 'Attach to process',
+    type = 'node2',
+    request = 'attach',
+    processId = require 'dap.utils'.pick_process,
+  },
+}
+
 
 -- formatters
 local formatters = require "lvim.lsp.null-ls.formatters"
