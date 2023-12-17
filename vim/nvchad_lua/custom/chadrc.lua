9e5fd1ac0f884bc5d26aa09699fd2832f04ae27b
taotao7 <moca_tao7@foxmail.com>
2023-12-17 15:33:51 +0800

    feat: change to use nvchad framwork

---
+++ vim/nvchad_lua/custom/chadrc.lua
@@ -0,0 +1,26 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "onedark",
+  theme_toggle = { "onedark", "one_light" },
+  statusline = {
+    theme = "vscode_colored",
+  },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+-- normal settings
+vim.g.mapleader = ","
+
+return M
---
+++ vim/nvchad_lua/custom/configs/overrides.lua
@@ -0,0 +1,70 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "vue",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "json",
+    "go",
+    "python",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+    "tailwindcss-language-server",
+    "eslint-lsp",
+    "vue-language-server",
+
+    -- python
+    "pyright",
+
+    -- golang
+    "gopls",
+    -- c/c++
+    "clangd",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
