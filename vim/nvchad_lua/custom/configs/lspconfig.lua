4e8f1b4e5f882b668bc39dcd4757c6da1c6a2e2b
taotao7 <moca_tao7@foxmail.com>
2024-01-23 17:53:18 +0800

    feat: change lsp none-ls config

--- vim/nvchad_lua/custom/configs/lspconfig.lua
+++ vim/nvchad_lua/custom/configs/lspconfig.lua
@@ -1,24 +1,20 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
 local servers = {
   "html",
   "cssls",
   "tsserver",
-  "pyright",
-  -- "gopls",
-  "clangd",
   "tailwindcss",
   "volar",
-  "eslint",
 }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  lspconfig[lsp].setup({
     on_attach = on_attach,
     capabilities = capabilities,
-  }
+  })
 end
--- vim/nvchad_lua/custom/configs/null-ls.lua
+++ vim/nvchad_lua/custom/configs/null-ls.lua
@@ -1,4 +1,4 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 
 local b = null_ls.builtins
 
@@ -7,38 +7,39 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local async_formatting = function(bufnr)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
 
-  vim.lsp.buf_request(bufnr, "textDocument/formatting", vim.lsp.util.make_formatting_params {}, function(err, res, ctx)
-    if err then
-      local err_msg = type(err) == "string" and err or err.message
-      -- you can modify the log message / level (or ignore it completely)
-      vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
-      return
-    end
+  vim.lsp.buf_request(
+    bufnr,
+    "textDocument/formatting",
+    vim.lsp.util.make_formatting_params({}),
+    function(err, res, ctx)
+      if err then
+        local err_msg = type(err) == "string" and err or err.message
+        -- you can modify the log message / level (or ignore it completely)
+        vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+        return
+      end
 
-    -- don't apply results if buffer is unloaded or has been modified
-    if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
-      return
-    end
+      -- don't apply results if buffer is unloaded or has been modified
+      if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+        return
+      end
 
-    if res then
-      local client = vim.lsp.get_client_by_id(ctx.client_id)
-      vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-      vim.api.nvim_buf_call(bufnr, function()
-        vim.cmd "silent noautocmd update"
-      end)
+      if res then
+        local client = vim.lsp.get_client_by_id(ctx.client_id)
+        vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+        vim.api.nvim_buf_call(bufnr, function()
+          vim.cmd("silent noautocmd update")
+        end)
+      end
     end
-  end)
+  )
 end
 
 local sources = {
-  b.diagnostics.eslint.with {
-    filetypes = { "html", "markdown", "css", "typescript", "javascriptreact", "typescriptreact", "javascript", "vue" },
-  },
+  b.diagnostics.eslint_d,
   -- webdev stuff
   -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with {
-    filetypes = { "html", "markdown", "css", "typescript", "javascriptreact", "typescriptreact", "javascript", "vue" },
-  }, -- so prettier works only on these filetypes
+  b.formatting.prettierd, -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
@@ -47,12 +48,12 @@ local sources = {
   b.formatting.clang_format,
 }
 
-null_ls.setup {
+null_ls.setup({
   debug = true,
   sources = sources,
   on_attach = function(client, bufnr)
-    if client.supports_method "textDocument/formatting" then
-      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
       vim.api.nvim_create_autocmd("BufWritePost", {
         group = augroup,
         buffer = bufnr,
@@ -62,4 +63,4 @@ null_ls.setup {
       })
     end
   end,
-}
+})
