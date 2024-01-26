44db0e185337cb485d6eeb2dc5d4e121c74a085e
taotao7 <moca_tao7@foxmail.com>
2024-01-26 15:41:32 +0800

    feat: add format

--- vim/nvchad_lua/custom/configs/null-ls.lua
+++ vim/nvchad_lua/custom/configs/null-ls.lua
@@ -10,14 +10,19 @@ local sources = {
 
 null_ls.setup {
   sources = sources,
-  on_attach = function(client, bufnr)
-    if client.supports_method "textDocument/formatting" then
+  on_attach = function(current_client, bufnr)
+    if current_client.supports_method "textDocument/formatting" then
       vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format { async = false }
+          vim.lsp.buf.format {
+            filter = function(client)
+              return client.name == "null-ls"
+            end,
+            bufnr = bufnr,
+          }
         end,
       })
     end
