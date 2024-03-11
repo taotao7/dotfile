009be427dfeda94649a51c36b6711b161e8743e4
taotao7 <moca_tao7@foxmail.com>
2024-03-11 12:10:12 +0800

    feat: add astronvim config

---
+++ vim/astronvim/user/plugins/community.lua
@@ -0,0 +1,28 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of importing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  --  pack
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.bash" },
+  -- motion
+  { import = "astrocommunity.motion.hop-nvim" },
+  -- lsp
+  { import = "astrocommunity.lsp.garbage-day-nvim" },
+  { import = "astrocommunity.lsp.lsp-lens-nvim" },
+  { import = "astrocommunity.lsp.lsp-inlayhints-nvim" },
+  -- indent
+  { import = "astrocommunity.indent/indent-blankline-nvim" },
+}
