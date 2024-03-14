46c18a3a20439488875206c445c0d743eee0f45f
taotao7 <moca_tao7@foxmail.com>
2024-03-14 17:13:52 +0800

    feat: remove csharp support

--- vim/astronvim/user/plugins/community.lua
+++ vim/astronvim/user/plugins/community.lua
@@ -22,7 +22,7 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.cs" },
+  -- { import = "astrocommunity.pack.cs" },
   -- { import = "astrocommunity.pack.markdown" },
   -- motion
   { import = "astrocommunity.motion.hop-nvim" },
