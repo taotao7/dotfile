fd71c93f22d930cf533e5c18bba55c7a938f6eab
taotao7 <moca_tao7@foxmail.com>
2024-03-18 18:20:53 +0800

    feat: warp line

--- vim/astronvim/user/options.lua
+++ vim/astronvim/user/options.lua
@@ -3,10 +3,10 @@ return {
   opt = {
     -- set to true or false etc.
     relativenumber = true, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-    wrap = false, -- sets vim.opt.wrap
+    number = true,         -- sets vim.opt.number
+    spell = false,         -- sets vim.opt.spell
+    signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
+    wrap = true,           -- sets vim.opt.wrap
     guicursor = table.concat({
       "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
       "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
@@ -15,13 +15,13 @@ return {
   },
   g = {
     mapleader = ",",
-    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true, -- enable completion at start
-    autopairs_enabled = true, -- enable autopairs at start
-    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    autoformat_enabled = true,       -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+    cmp_enabled = true,              -- enable completion at start
+    autopairs_enabled = true,        -- enable autopairs at start
+    diagnostics_mode = 3,            -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+    icons_enabled = true,            -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-    resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
+    resession_enabled = false,       -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
     relativenumber = true,
   },
 }
