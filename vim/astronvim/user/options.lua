b38bf5d5ea40780311b0d4f2c8caded1fd37861f
taotao7 <moca_tao7@foxmail.com>
2024-03-18 14:25:40 +0800

    chore: update cursor pointer

--- vim/astronvim/user/options.lua
+++ vim/astronvim/user/options.lua
@@ -7,6 +7,11 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    guicursor = table.concat({
+      "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+      "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+      "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100",
+    }, ","),
   },
   g = {
     mapleader = ",",
