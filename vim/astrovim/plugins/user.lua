d6cf9cf1bf2d0c3ad252e2e9ee22bc4c8ee391d9
taotao7 <moca_tao7@foxmail.com>
2024-04-02 17:37:45 +0800

    feat: remove discord

--- vim/astrovim/plugins/user.lua
+++ vim/astrovim/plugins/user.lua
@@ -8,12 +8,12 @@ return {
 
   -- == Examples of Adding Plugins ==
 
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function() require("lsp_signature").setup() end,
+  -- },
 
   -- == Examples of Overriding Plugins ==
 
