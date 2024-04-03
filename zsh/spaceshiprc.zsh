ea75f840b5e1d7f5325333b4d3042cb78ffe0801
taotao7 <moca_tao7@foxmail.com>
2024-04-03 14:27:45 +0800

    feat: update zshrc
    add spaceship prompt
    add zsh plugin

---
+++ zsh/spaceshiprc.zsh
@@ -0,0 +1,11 @@
+# Display time
+SPACESHIP_TIME_SHOW=true
+
+# Display username always
+SPACESHIP_USER_SHOW=always
+
+# battery
+SPACESHIP_BATTERY_SHOW=always
+
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=true
