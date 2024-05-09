4a698c411e57504d48b2267c375239f91bc49046
taotao7 <moca_tao7@foxmail.com>
2024-05-09 15:30:40 +0800

    feat: change cmp to coq

--- vim/astrovim/plugins/user.lua
+++ vim/astrovim/plugins/user.lua
@@ -38,16 +38,16 @@ return {
   { "max397574/better-escape.nvim", enabled = true },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-      luasnip.filetype_extend("typescript", { "typescriptreact" })
-    end,
-  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   config = function(plugin, opts)
+  --     require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
+  --     local luasnip = require "luasnip"
+  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
+  --     luasnip.filetype_extend("typescript", { "typescriptreact" })
+  --   end,
+  -- },
 
   {
     "windwp/nvim-autopairs",
