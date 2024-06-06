788063334980d15623211d60a582070adc76a50d
taotao7 <moca_tao7@foxmail.com>
2024-06-06 10:24:36 +0800

    feat: update solarized

--- vim/astrovim/plugins/astroui.lua
+++ vim/astrovim/plugins/astroui.lua
@@ -3,7 +3,7 @@ return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    colorscheme = "github_dark",
+    colorscheme = "everforest",
     status = {
       separators = {
         left = { "", " " }, -- separator for the left side of the statusline
