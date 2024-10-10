84266c13ebff2e1fc73f1d26f0924dae8b2efb4b
taotao7 <moca_tao7@foxmail.com>
2024-10-10 16:58:20 +0800

    feat: add lsp config

--- vim/astrovim/plugins/astrolsp.lua
+++ vim/astrovim/plugins/astrolsp.lua
@@ -1,21 +1,93 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrolsp",
+  tag = "stable",
+  pin = true,
+
   ---@type AstroLSPOpts
   opts = {
-    -- Configuration table of features provided by AstroLSP
-    features = {
-      codelens = true, -- enable/disable codelens refresh on start
-      inlay_hints = false, -- enable/disable inlay hints on start
-      semantic_tokens = true, -- enable/disable semantic token highlighting
+    ---@type any
+    config = {
+      eslint = {
+        filetypes = {
+          "javascript",
+          "javascript.jsx",
+          "javascriptreact",
+          "typescript",
+          "typescript.tsx",
+          "typescriptreact",
+          "vue",
+        },
+      },
+      volar = {
+        init_options = {
+          vue = {
+            hybridMode = false,
+          },
+        },
+        settings = {
+          typescript = {
+            updateImportsOnFileMove = { enabled = "always" },
+            inlayHints = {
+              parameterNames = { enabled = "all" },
+              parameterTypes = { enabled = true },
+              variableTypes = { enabled = true },
+              propertyDeclarationTypes = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              enumMemberValues = { enabled = true },
+            },
+          },
+          javascript = {
+            updateImportsOnFileMove = { enabled = "always" },
+            inlayHints = {
+              parameterNames = { enabled = "literals" },
+              parameterTypes = { enabled = true },
+              variableTypes = { enabled = true },
+              propertyDeclarationTypes = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              enumMemberValues = { enabled = true },
+            },
+          },
+        },
+      },
+      vtsls = {
+        settings = {
+          vtsls = {
+            tsserver = {
+              globalPlugins = {},
+            },
+          },
+        },
+        before_init = function(_, config)
+          local astrocore_ok, astrocore = pcall(require, "astrocore")
+          local registry_ok, registry = pcall(require, "mason-registry")
+          if not astrocore_ok or not registry_ok then return end
+
+          local vuels = registry.get_package "vue-language-server"
+
+          if vuels:is_installed() then
+            local volar_install_path = vuels:get_install_path() .. "/node_modules/@vue/language-server"
+
+            local vue_plugin_config = {
+              name = "@vue/typescript-plugin",
+              location = volar_install_path,
+              languages = { "vue" },
+              configNamespace = "typescript",
+              enableForWorkspaceTypeScriptVersions = true,
+            }
+
+            astrocore.list_insert_unique(config.settings.vtsls.tsserver.globalPlugins, { vue_plugin_config })
+          end
+        end,
+      },
     },
-    -- customize lsp formatting options
+
     formatting = {
       async = false,
       format_on_save = { enabled = true },
 
       filter = function(client)
-        local ignored_clients = { "jsonls", "volar" }
+        local ignored_clients = { "jsonls", "tsserver", "vtsls", "volar" }
 
         for _, client_name in ipairs(ignored_clients) do
           if client_name == client.name then return false end
