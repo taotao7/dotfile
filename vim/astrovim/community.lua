83bd1a796051618b573045e3155c3a6b54e90a68
taotao7 <moca_tao7@foxmail.com>
2024-12-21 21:13:41 +0800

    feat: specification vscode

--- vim/astrovim/community.lua
+++ vim/astrovim/community.lua
@@ -10,7 +10,7 @@ return {
   -- { import = "astrocommunity.diagnostics.error-lens-nvim" },
   -- { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
   -- { import = "astrocommunity.diagnostics.trouble-nvim" },
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
   { import = "astrocommunity.pack.lua" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
