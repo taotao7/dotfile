08665d4160cf0fe11f56358bd4a0933942eb0cb6
taotao7 <moca_tao7@foxmail.com>
2025-01-23 10:06:04 +0800

    refactor: update Deepseek model from "deepseek-coder" to "deepseek-reasoner"

--- vim/astrovim/lua/plugins/avante.lua
+++ vim/astrovim/lua/plugins/avante.lua
@@ -88,7 +88,7 @@ return {
         __inherited_from = "openai",
         api_key_name = "DEEPSEEK_API_KEY",
         endpoint = "https://api.deepseek.com",
-        model = "deepseek-coder",
+        model = "deepseek-reasoner",
       },
     },
     claude = {
