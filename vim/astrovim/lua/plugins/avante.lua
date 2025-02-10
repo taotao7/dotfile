be5c94e5c07a7e7bd5404ec639225b3721e308d0
taotao7 <moca_tao7@foxmail.com>
2025-02-10 13:52:50 +0800

    chore: update plugin versions and refactor avante.lua configuration

--- vim/astrovim/lua/plugins/avante.lua
+++ vim/astrovim/lua/plugins/avante.lua
@@ -1,6 +1,9 @@
+local prefix = "<Leader>a"
 return {
   "yetone/avante.nvim",
-  build = ":AvanteBuild",
+  build = vim.fn.has "win32" == 1 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+    or "make",
+  event = "User AstroFile", -- load on file open because Avante manages it's own bindings
   -- init = function()
   --   local config = require "avante.config"
   --   config.auto_suggestions_provider = "deepseek"
@@ -16,79 +19,39 @@ return {
     "AvanteClear",
   },
   dependencies = {
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
+    "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    {
-      "AstroNvim/astrocore",
-      opts = function(_, opts)
-        local maps = assert(opts.mappings)
-        local prefix = "<Leader>a"
-
-        maps.n[prefix] = { desc = "Avante functionalities" }
-
-        maps.n[prefix .. "a"] = { function() require("avante.api").ask() end, desc = "Avante ask" }
-        maps.v[prefix .. "a"] = { function() require("avante.api").ask() end, desc = "Avante ask" }
-
-        maps.v[prefix .. "r"] = { function() require("avante.api").refresh() end, desc = "Avante refresh" }
-
-        maps.n[prefix .. "e"] = { function() require("avante.api").edit() end, desc = "Avante edit" }
-        maps.v[prefix .. "e"] = { function() require("avante.api").edit() end, desc = "Avante edit" }
-
-        -- the following key bindings do not have an official api implementation
-        maps.n.co = { "<Plug>(AvanteConflictOurs)", desc = "Choose ours", expr = true }
-        maps.v.co = { "<Plug>(AvanteConflictOurs)", desc = "Choose ours", expr = true }
-
-        maps.n.ct = { "<Plug>(AvanteConflictTheirs)", desc = "Choose theirs", expr = true }
-        maps.v.ct = { "<Plug>(AvanteConflictTheirs)", desc = "Choose theirs", expr = true }
-
-        maps.n.ca = { "<Plug>(AvanteConflictAllTheirs)", desc = "Choose all theirs", expr = true }
-        maps.v.ca = { "<Plug>(AvanteConflictAllTheirs)", desc = "Choose all theirs", expr = true }
-
-        maps.n.cb = { "<Plug>(AvanteConflictBoth)", desc = "Choose both", expr = true }
-        maps.v.cb = { "<Plug>(AvanteConflictBoth)", desc = "Choose both", expr = true }
-
-        maps.n.cc = { "<Plug>(AvanteConflictCursor)", desc = "Choose cursor", expr = true }
-        maps.v.cc = { "<Plug>(AvanteConflictCursor)", desc = "Choose cursor", expr = true }
-
-        maps.n["]x"] = { "<Plug>(AvanteConflictPrevConflict)", desc = "Move to previous conflict", expr = true }
-
-        maps.n["[x"] = { "<Plug>(AvanteConflictNextConflict)", desc = "Move to next conflict", expr = true }
-      end,
-    },
+    { "AstroNvim/astrocore", opts = function(_, opts) opts.mappings.n[prefix] = { desc = " Avante" } end },
   },
   opts = {
+    mappings = {
+      ask = prefix .. "<CR>",
+      edit = prefix .. "e",
+      refresh = prefix .. "r",
+      focus = prefix .. "f",
+      toggle = {
+        default = prefix .. "t",
+        debug = prefix .. "d",
+        hint = prefix .. "h",
+        suggestion = prefix .. "s",
+        repomap = prefix .. "R",
+      },
+      diff = {
+        next = "]c",
+        prev = "[c",
+      },
+      files = {
+        add_current = prefix .. ".",
+      },
+    },
     provider = "deepseek",
     vendors = {
       deepseek = {
         __inherited_from = "openai",
         api_key_name = "DEEPSEEK_API_KEY",
         endpoint = "https://api.deepseek.com",
-        model = "deepseek-reasoner",
+        model = "deepseek-chat",
       },
     },
     claude = {
@@ -106,5 +69,38 @@ return {
       minimize_diff = true,
     },
   },
-  specs = {},
+  specs = {
+    { "AstroNvim/astroui", opts = { icons = { Avante = "" } } },
+    { -- if copilot.lua is available, default to copilot provider
+      "zbirenbaum/copilot.lua",
+      optional = true,
+      specs = {
+        {
+          "yetone/avante.nvim",
+          opts = {
+            provider = "copilot",
+            auto_suggestions_provider = "copilot",
+          },
+        },
+      },
+    },
+    {
+      -- make sure `Avante` is added as a filetype
+      "MeanderingProgrammer/render-markdown.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if not opts.file_types then opts.file_types = { "markdown" } end
+        opts.file_types = require("astrocore").list_insert_unique(opts.file_types, { "Avante" })
+      end,
+    },
+    {
+      -- make sure `Avante` is added as a filetype
+      "OXY2DEV/markview.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if not opts.filetypes then opts.filetypes = { "markdown", "quarto", "rmd" } end
+        opts.filetypes = require("astrocore").list_insert_unique(opts.filetypes, { "Avante" })
+      end,
+    },
+  },
 }
