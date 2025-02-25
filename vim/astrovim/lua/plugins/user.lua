c5b81fe3feeb4a1b44663b33a562e149d971185d
taotao7 <moca_tao7@foxmail.com>
2025-02-25 15:48:54 +0800

    chore: Update plugin versions and modify monokai-pro filter to "light"

--- vim/astrovim/lua/plugins/user.lua
+++ vim/astrovim/lua/plugins/user.lua
@@ -1,32 +1,32 @@
 ---@type LazySpec
 return {
 
-  -- customize alpha options
-  {
-    "goolord/alpha-nvim",
-    opts = function(_, opts)
-      -- customize the dashboard header
-      opts.section.header.val = {
-        " ████████╗ █████╗  ██████╗       ███████╗",
-        " ╚══██╔══╝██╔══██╗██╔═══██╗      ██╔════╝",
-        "    ██║   ███████║██║   ██║█████╗███████╗",
-        "    ██║   ██╔══██║██║   ██║╚════╝╚════██║",
-        "    ██║   ██║  ██║╚██████╔╝      ███████║",
-        "    ╚═╝   ╚═╝  ╚═╝ ╚═════╝       ╚══════╝",
-      }
-      return opts
-    end,
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    enabled = false,
-  },
-  {
-    "loctvl842/monokai-pro.nvim",
-    config = function()
-      require("monokai-pro").setup {
-        filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
-      }
-    end,
-  },
+	-- customize alpha options
+	{
+		"goolord/alpha-nvim",
+		opts = function(_, opts)
+			-- customize the dashboard header
+			opts.section.header.val = {
+				" ████████╗ █████╗  ██████╗       ███████╗",
+				" ╚══██╔══╝██╔══██╗██╔═══██╗      ██╔════╝",
+				"    ██║   ███████║██║   ██║█████╗███████╗",
+				"    ██║   ██╔══██║██║   ██║╚════╝╚════██║",
+				"    ██║   ██║  ██║╚██████╔╝      ███████║",
+				"    ╚═╝   ╚═╝  ╚═╝ ╚═════╝       ╚══════╝",
+			}
+			return opts
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		enabled = false,
+	},
+	{
+		"loctvl842/monokai-pro.nvim",
+		config = function()
+			require("monokai-pro").setup({
+				filter = "light", -- classic | octagon | pro | machine | ristretto | spectrum
+			})
+		end,
+	},
 }
