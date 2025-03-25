bcd3d24e2893202317fdf3d683322aab65063966
taotao7 <moca_tao7@foxmail.com>
2025-03-25 12:00:39 +0800

    chore: update plugins and switch to horizon theme

--- vim/astrovim/lua/plugins/avante.lua
+++ vim/astrovim/lua/plugins/avante.lua
@@ -51,12 +51,19 @@ return {
 			},
 		},
 		provider = "deepseek",
+		-- auto_suggestions_provider = "openrouter",
 		vendors = {
 			deepseek = {
 				__inherited_from = "openai",
 				api_key_name = "DEEPSEEK_API_KEY",
 				endpoint = "https://api.deepseek.com",
 				model = "deepseek-chat",
+			},
+			openrouter = {
+				__inherited_from = "openai",
+				endpoint = "https://openrouter.ai/api/v1",
+				api_key_name = "OPENROUTER_API_KEY",
+				model = "anthropic/claude-3.7-sonnet",
 			},
 		},
 		claude = {
@@ -66,12 +73,19 @@ return {
 			max_tokens = 8192,
 		},
 		behaviour = {
-			auto_suggestions = false, -- Experimental stage
+			auto_suggestions = false,
 			auto_set_highlight_group = true,
 			auto_set_keymaps = true,
 			auto_apply_diff_after_generation = false,
 			support_paste_from_clipboard = false,
 			minimize_diff = true,
+			enable_token_counting = true,
+			enable_cursor_planning_mode = false,
+			enable_claude_text_editor_tool_mode = false,
+		},
+		suggestion = {
+			debounce = 600,
+			throttle = 600,
 		},
 	},
 	specs = {
