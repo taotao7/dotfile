9f3af36ee211fc1328d8ef65d4a732455a965afa
taotao7 <moca_tao7@foxmail.com>
2025-04-22 18:47:15 +0800

    feat: add key mappings for Augment chat commands

---
+++ vim/astrovim/lua/plugins/mapping.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        -- first key is the mode
+        n = {
+          -- second key is the lefthand side of the map
+          -- mappings seen under group name "Buffer"
+          ["<Leader>ac"] = { "<cmd>Augment chat<cr>", desc = "Chat" },
+          ["<Leader>an"] = { "<cmd>Augment chat-new<cr>", desc = "new Chat" },
+          ["<Leader>at"] = { "<cmd>Augment chat-toggle<cr>", desc = "Toggle Chat" },
+          -- tables with just a `desc` key will be registered with which-key if it's installed
+          -- this is useful for naming menus
+          ["<Leader>b"] = { desc = "Buffers" },
+          -- quick save
+          -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+        },
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
+        },
+      },
+    },
+  },
+}
