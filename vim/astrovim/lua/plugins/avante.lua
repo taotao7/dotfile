7fe141677a2f798a7f931aade7679048b00adf89
taotao7 <moca_tao7@foxmail.com>
2025-08-04 14:17:07 +0800

    chore: update plugin versions and configs

--- vim/astrovim/lua/plugins/avante.lua
+++ vim/astrovim/lua/plugins/avante.lua
@@ -2,7 +2,7 @@ local prefix = "<Leader>a"
 return {
 	"yetone/avante.nvim",
 	build = vim.fn.has("win32") == 1 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-		or "BUILD_FROM_SOURCE=true make",
+			or "BUILD_FROM_SOURCE=true make",
 	event = "User AstroFile", -- load on file open because Avante manages it's own bindings
 	-- init = function()
 	--   local config = require "avante.config"
@@ -50,7 +50,7 @@ return {
 				add_current = prefix .. ".",
 			},
 		},
-		provider = "zhipu",
+		provider = "moonshot",
 		-- auto_suggestions_provider = "openrouter",
 		providers = {
 			deepseek = {
--- vim/astrovim/lua/plugins/user.lua
+++ vim/astrovim/lua/plugins/user.lua
@@ -5,113 +5,137 @@
 ---@type LazySpec
 return {
 
-  -- == Examples of Adding Plugins ==
-  {
-    "folke/snacks.nvim",
-    config = function(_, opts)
-      opts.indent = {
-        enable = false,
-      }
-      -- require("snacks").setup({
-    end,
-  },
+	-- == Examples of Adding Plugins ==
+	{
+		"folke/snacks.nvim",
+		config = function(_, opts)
+			opts.indent = {
+				enable = false,
+			}
+			-- require("snacks").setup({
+		end,
+	},
 
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function()
-      require("lsp_signature").setup()
-    end,
-  },
-  {
-    "folke/snacks.nvim",
-    opts = {
-      dashboard = {
-        preset = {
-          header = table.concat({
-            " ████████╗ █████╗  ██████╗       ███████╗",
-            " ╚══██╔══╝██╔══██╗██╔═══██╗      ██╔════╝",
-            "    ██║   ███████║██║   ██║█████╗███████╗",
-            "    ██║   ██╔══██║██║   ██║╚════╝╚════██║",
-            "    ██║   ██║  ██║╚██████╔╝      ███████║",
-            "    ╚═╝   ╚═╝  ╚═╝ ╚═════╝       ╚══════╝",
-          }, "\n"),
-        },
-      },
-    },
-  },
+	"andweeb/presence.nvim",
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "BufRead",
+		config = function()
+			require("lsp_signature").setup()
+		end,
+	},
+	{
+		"folke/snacks.nvim",
+		opts = {
+			dashboard = {
+				preset = {
+					header = table.concat({
+						" ████████╗ █████╗  ██████╗       ███████╗",
+						" ╚══██╔══╝██╔══██╗██╔═══██╗      ██╔════╝",
+						"    ██║   ███████║██║   ██║█████╗███████╗",
+						"    ██║   ██╔══██║██║   ██║╚════╝╚════██║",
+						"    ██║   ██║  ██║╚██████╔╝      ███████║",
+						"    ╚═╝   ╚═╝  ╚═╝ ╚═════╝       ╚══════╝",
+					}, "\n"),
+				},
+			},
+		},
+	},
 
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require("astronvim.plugins.configs.luasnip")(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require("luasnip")
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-  },
+	-- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+	{
+		"L3MON4D3/LuaSnip",
+		config = function(plugin, opts)
+			require("astronvim.plugins.configs.luasnip")(plugin, opts) -- include the default astronvim config that calls the setup call
+			-- add more custom luasnip configuration such as filetype extend or custom snippets
+			local luasnip = require("luasnip")
+			luasnip.filetype_extend("javascript", { "javascriptreact" })
+		end,
+	},
 
-  {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require("astronvim.plugins.configs.nvim-autopairs")(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require("nvim-autopairs")
-      local Rule = require("nvim-autopairs.rule")
-      local cond = require("nvim-autopairs.conds")
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-          -- don't add a pair if the next character is %
-              :with_pair(cond.not_after_regex("%%"))
-          -- don't add a pair if  the previous character is xxx
-              :with_pair(
-                cond.not_before_regex("xxx", 3)
-              )
-          -- don't move right when repeat character
-              :with_move(cond.none())
-          -- don't delete if the next character is xx
-              :with_del(cond.not_after_regex("xx"))
-          -- disable adding a newline when you press <cr>
-              :with_cr(cond.none()),
-        },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
-    end,
-  },
-  {
-    "talha-akram/noctis.nvim",
-    enabled = true,
-  },
-  {
-    "junegunn/seoul256.vim",
-    enabled = true,
-  },
-  { "akinsho/horizon.nvim",           version = "*",  enabled = true },
-  {
-    "augmentcode/augment.vim",
-    config = function()
-      vim.g.augment_workspace_folders = { "/home/tao/workspace" }
-      -- require("snacks").setup({
-    end,
-    enabled = false,
-  },
-  { "EdenEast/nightfox.nvim",         enabled = false },
-  -- {
-  --   "augmentcode/augment.vim",
-  --   config = function(plugin, opts)
-  --   vim.g.augment_workspace_folders = ["/Users/tao/workspace/org-management-temp"]
-  -- end,
-  -- },
-  { "yetone/avante.nvim",             enabled = true },
-  { "supermaven-inc/supermaven-nvim", enabled = true },
-  {
-    "rebelot/heirline.nvim",
-    opts = function(_, opts)
-      opts.winbar = nil
-    end,
-  },
+	{
+		"windwp/nvim-autopairs",
+		config = function(plugin, opts)
+			require("astronvim.plugins.configs.nvim-autopairs")(plugin, opts) -- include the default astronvim config that calls the setup call
+			-- add more custom autopairs configuration such as custom rules
+			local npairs = require("nvim-autopairs")
+			local Rule = require("nvim-autopairs.rule")
+			local cond = require("nvim-autopairs.conds")
+			npairs.add_rules(
+				{
+					Rule("$", "$", { "tex", "latex" })
+					-- don't add a pair if the next character is %
+							:with_pair(cond.not_after_regex("%%"))
+					-- don't add a pair if  the previous character is xxx
+							:with_pair(
+								cond.not_before_regex("xxx", 3)
+							)
+					-- don't move right when repeat character
+							:with_move(cond.none())
+					-- don't delete if the next character is xx
+							:with_del(cond.not_after_regex("xx"))
+					-- disable adding a newline when you press <cr>
+							:with_cr(cond.none()),
+				},
+				-- disable for .vim files, but it work for another filetypes
+				Rule("a", "a", "-vim")
+			)
+		end,
+	},
+	{
+		"talha-akram/noctis.nvim",
+		enabled = true,
+	},
+	{
+		"junegunn/seoul256.vim",
+		enabled = true,
+	},
+	{ "akinsho/horizon.nvim",   version = "*",  enabled = true },
+	{
+		"augmentcode/augment.vim",
+		config = function()
+			vim.g.augment_workspace_folders = { "/home/tao/workspace" }
+			-- require("snacks").setup({
+		end,
+		enabled = false,
+	},
+	{ "EdenEast/nightfox.nvim", enabled = false },
+	-- {
+	--   "augmentcode/augment.vim",
+	--   config = function(plugin, opts)
+	--   vim.g.augment_workspace_folders = ["/Users/tao/workspace/org-management-temp"]
+	-- end,
+	-- },
+	{ "yetone/avante.nvim",     enabled = false },
+	{
+		"coder/claudecode.nvim",
+		dependencies = { "folke/snacks.nvim" },
+		config = true,
+		keys = {
+			{ "<leader>a",  nil,                              desc = "AI/Claude Code" },
+			{ "<leader>ac", "<cmd>ClaudeCode<cr>",            desc = "Toggle Claude" },
+			{ "<leader>af", "<cmd>ClaudeCodeFocus<cr>",       desc = "Focus Claude" },
+			{ "<leader>ar", "<cmd>ClaudeCode --resume<cr>",   desc = "Resume Claude" },
+			{ "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+			{ "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
+			{ "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>",       desc = "Add current buffer" },
+			{ "<leader>as", "<cmd>ClaudeCodeSend<cr>",        mode = "v",                  desc = "Send to Claude" },
+			{
+				"<leader>as",
+				"<cmd>ClaudeCodeTreeAdd<cr>",
+				desc = "Add file",
+				ft = { "NvimTree", "neo-tree", "oil" },
+			},
+			-- Diff management
+			{ "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+			{ "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>",   desc = "Deny diff" },
+		},
+	},
+	{ "supermaven-inc/supermaven-nvim", enabled = true },
+	{
+		"rebelot/heirline.nvim",
+		opts = function(_, opts)
+			opts.winbar = nil
+		end,
+	},
 }
