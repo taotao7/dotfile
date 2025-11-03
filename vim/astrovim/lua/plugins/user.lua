1d798b514c1e3b66b0feae773aa231be1acf6d27
taotao7 <moca_tao7@foxmail.com>
2025-11-03 10:45:30 +0800

    style: fix indentation and spacing in user.lua

--- vim/astrovim/lua/plugins/user.lua
+++ vim/astrovim/lua/plugins/user.lua
@@ -55,18 +55,18 @@ return {
 			npairs.add_rules(
 				{
 					Rule("$", "$", { "tex", "latex" })
-						-- don't add a pair if the next character is %
-						:with_pair(cond.not_after_regex("%%"))
-						-- don't add a pair if  the previous character is xxx
-						:with_pair(
-							cond.not_before_regex("xxx", 3)
-						)
-						-- don't move right when repeat character
-						:with_move(cond.none())
-						-- don't delete if the next character is xx
-						:with_del(cond.not_after_regex("xx"))
-						-- disable adding a newline when you press <cr>
-						:with_cr(cond.none()),
+					-- don't add a pair if the next character is %
+							:with_pair(cond.not_after_regex("%%"))
+					-- don't add a pair if  the previous character is xxx
+							:with_pair(
+								cond.not_before_regex("xxx", 3)
+							)
+					-- don't move right when repeat character
+							:with_move(cond.none())
+					-- don't delete if the next character is xx
+							:with_del(cond.not_after_regex("xx"))
+					-- disable adding a newline when you press <cr>
+							:with_cr(cond.none()),
 				},
 				-- disable for .vim files, but it work for another filetypes
 				Rule("a", "a", "-vim")
@@ -85,7 +85,7 @@ return {
 		"p00f/alabaster.nvim",
 		enabled = false,
 	},
-	{ "akinsho/horizon.nvim", version = "*", enabled = true },
+	{ "akinsho/horizon.nvim",   version = "*",  enabled = true },
 	{
 		"augmentcode/augment.vim",
 		config = function()
@@ -101,11 +101,11 @@ return {
 	--   vim.g.augment_workspace_folders = ["/Users/tao/workspace/org-management-temp"]
 	-- end,
 	-- },
-	{ "yetone/avante.nvim", enabled = false },
+	{ "yetone/avante.nvim",     enabled = false },
 	{
 		"coder/claudecode.nvim",
 		dependencies = { "folke/snacks.nvim" },
-		enabled = false,
+		enabled = true,
 		config = true,
 		-- keys = {
 		-- 	{ "<leader>a",  nil,                              desc = "AI/Claude Code" },
